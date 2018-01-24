c26778a686c9897924312ad7bab60468c0d7e756
pradeep39t <pradeep.panigrahy_nist@yahoo.com>
2018-01-24 16:17:15 +0530

    Update Case_Creation.js

--- Case_Creation.js
+++ Case_Creation.js
@@ -43,8 +43,9 @@ function myFunction(){
 	}	
 	if(document.getElementById('PRINTTITLE').innerHTML=='Register Program - Program'){
 		document.getElementById('maFormCd').value='HCS-250';
-		//document.getElementById('cash').checked;
-		//document.getElementById('SNAP').checked;		
+		document.getElementById('Medicaid').checked;
+		document.getElementById('cash').checked;
+		document.getElementById('SNAP').checked;		
 	}
 	if(document.getElementById('PRINTTITLE').innerHTML=='Register Program - Expedited Screening'){
 		document.getElementById('expeditedCountableGrossIncome').value='200';
